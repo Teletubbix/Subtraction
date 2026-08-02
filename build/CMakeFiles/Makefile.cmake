48981d278ea4ccc79c7c752a8300882d048a2279
Teletubbix <3461530375@qq.com>
2026-08-02 22:52:31 +0800

    Third Submission

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -60,5 +60,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/subtraction.dir/DependInfo.cmake"
+  "CMakeFiles/subtraction_calc.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/subtraction_calc.dir/src/math_subtract.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/subtraction_calc.dir/src/math_subtract.c.obj: \
+ D:\VSCode\Code\C\Subtraction\src\math_subtract.c \
+ D:/Mingw/x86_64-w64-mingw32/include/stdio.h \
+ D:/Mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ D:/Mingw/x86_64-w64-mingw32/include/corecrt.h \
+ D:/Mingw/x86_64-w64-mingw32/include/_mingw.h \
+ D:/Mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/Mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/Mingw/x86_64-w64-mingw32/include/vadefs.h \
+ D:/Mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ D:/Mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ D:/Mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ D:/VSCode/Code/C/Subtraction/include/math_subtract.h
